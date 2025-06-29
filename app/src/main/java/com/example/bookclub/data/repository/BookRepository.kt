da72e66b7793223784ed4e27ec2bd2587909a9b0
Mazalll <121098349+Mazalll@users.noreply.github.com>
2025-06-29 17:21:29 +0300

    WE HAVE FIREBASE!!! now we have books too..

--- app/src/main/java/com/example/bookclub/data/repository/BookRepository.kt
+++ app/src/main/java/com/example/bookclub/data/repository/BookRepository.kt
@@ -1,14 +1,17 @@
 package com.example.bookclub.data.repository
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.bookclub.data.local_db.BookDatabase
 import com.example.bookclub.data.model.Book
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class BookRepository(application: Application) {
     private val bookDao = BookDatabase.getDatabase(application).bookDao()
+    private val firestore = FirebaseFirestore.getInstance()
 
     fun getBooks(): LiveData<List<Book>> = bookDao.getBooks()
 
@@ -16,6 +19,15 @@ class BookRepository(application: Application) {
 
     suspend fun addBook(book: Book) = withContext(Dispatchers.IO) {
         bookDao.addBook(book)
+
+        firestore.collection("books")
+            .add(book)
+            .addOnSuccessListener {
+                Log.d("Firestore", "Book saved successfully to Firestore")
+            }
+            .addOnFailureListener { e ->
+                Log.e("Firestore", "Error saving book to Firestore", e)
+            }
     }
 
     suspend fun update(book: Book) = withContext(Dispatchers.IO) {
